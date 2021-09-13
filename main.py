4386e3c0fd647e0601398863d7f68ceb0a767519
LAPTOP-D3VMQB6S\Gustav <gustavkeppler@gmail.com>
2021-09-13 18:01:08 +0200

    ets

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ def main(args):
     trainset = SemiDataset(args.dataset, args.data_root, MODE, args.crop_size, args.labeled_id_path)
     trainset.ids = 2 * trainset.ids if len(trainset.ids) < 200 else trainset.ids
     subset_indices = (0,1,2)
-    trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=True,
-                             pin_memory=True, num_workers=16, drop_last=True)#,sampler=torch.utils.data.SubsetRandomSampler(subset_indices))
+    trainloader = DataLoader(trainset, batch_size=args.batch_size, shuffle=False,
+                             pin_memory=True, num_workers=16, drop_last=True,sampler=torch.utils.data.SubsetRandomSampler(subset_indices))
 
     model, optimizer = init_basic_elems(args)
     print('\nParams: %.1fM' % count_params(model))
