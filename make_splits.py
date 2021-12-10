9f1e122b881befc97871096649d09cdcbd5fc222
LAPTOP-D3VMQB6S\Gustav <gustavkeppler@gmail.com>
2021-12-10 12:16:08 +0100

    changes indices in isic_dermo_dataset and new make_split file for yaml

--- make_splits.py
+++ make_splits.py
@@ -6,35 +6,46 @@ import os
 import random
 from os import listdir
 from os.path import isfile, join
-
+import yaml
 
 # %%
-dataset = r"pascal"
-split = 1/8
-path = (r"C:\Users\Gustav\Documents\GitHub\Masterarbeit\Datasets\%s\SegmentationClass" % (dataset))
-print(path)
+dataset = r"melanoma"
+val_split = 1/10
+splits = ["1/8", "1/4", "1/30"]
+path = (r"C:\Users\Gustav\source\repos\Masterarbeit\Datasets\%s" % (dataset))
 filelist = []
-csv = False
-csv_name = "ISIC-2017_Validation_Data_metadata.csv"
+csv = True
+csv_name = "ISIC-2017_Validation_Part3_GroundTruth.csv"
 
 if csv:
     csv_df = pd.read_csv(os.path.join(path, csv_name), delimiter = ",", sep=",")
     for row in csv_df.iterrows():
-        filelist.append("JPEGImages/%s.jpg labels/%s+_segmentation.png"% (row[1]["image_id"], row[1]["image_id"]))
+        filelist.append("JPEGImages/%s.jpg labels/%s_segmentation.png"% (row[1]["image_id"], row[1]["image_id"]))
 else:
     filelist = ["JPEGImages/%s.jpg SegmentationClass/%s"%(f[:-4],f) for f in listdir(path) if isfile(join(path, f))]
 
 print(filelist[:2])
-random.shuffle(filelist)
-len = len(filelist) 
-split = 1.0/8.0
+list_len = len(filelist) 
+# %%
+yaml_dict = {}
+for split in splits:
+    random.shuffle(filelist)
+    labeled_splitpoint = int(list_len*(1-float(eval(split)))*(1-val_split))
+    val_splitpoint = int(list_len*(1-val_split))
+    print(f'splitpoint for {split} in dataset with list_len {list_len} are {labeled_splitpoint} and {val_splitpoint}')
+    unlabeled_data = filelist[:labeled_splitpoint]
+    labeled_data = filelist[labeled_splitpoint:val_splitpoint]
+    val_data = filelist[val_splitpoint:]
+    yaml_dict[str(split)] = dict(
+        unlabeled=unlabeled_data,
+        labeled=labeled_data,
+        val=val_data)
+with open(f'dataset\splits\{dataset}\splits.yml', 'w+') as outfile:
+    yaml.dump(yaml_dict, outfile, default_flow_style=False)
 
 
-unlabeled_data = filelist[:int(len*(1-split))]
-labeled_data = filelist[int(len*(1-split)):-100]
-val_data = filelist[-100:]
 
-
+# %%
 text = ""
 for el in val_data:
     text += el+"\n"
